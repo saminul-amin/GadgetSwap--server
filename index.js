f06ecf89d874e7e5b54ec3f387b8d96fa606ef09
Md. Saminul Amin <saminul.amin@gmail.com>
2025-07-22 12:04:28 +0600

    JWT and MongoDB connection added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const port = process.env.PORT || 3000;
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // initializing middlewares
 app.use(cors({
@@ -19,5 +20,41 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser());
 
+// verifying jwt!
+const verifyJWT = (req, res, next) => {
+    const email = req?.body?.email;
+    const token = req?.cookies?.token;
+
+    if (!token) {
+        return res.send({ status: 401, message: "No token provided, authorization denied!" });
+    }
+
+    jwt.verify(token, process.env.ACCESS_JWT_SECRET, (error, decoded) => {
+        if (error) {
+            return res.send({ status: 402, message: "Invalid or expired token!" });
+        }
+        req.decoded_email = decoded?.data;
+        next();
+    });
+};
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ktxyk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+app.get('/', (req, res) => {
+    res.send('GadgetSwap Server is running...');
+})
+
+app.listen(port, () => {
+    console.log(`GadgetSwap is listening on port ${port}`);
+})
